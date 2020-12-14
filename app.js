38bc54fffa4805ea8d40c4aced6f0560695d2c3c
nikolaitrifonov0 <nikolaitrifonov0@gmail.com>
2020-12-14 20:17:27 +0200

    the default path now leads to home component

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 import {Router} from 'https://unpkg.com/@vaadin/router';
+import Home from './components/home.js';
 
-const outlet = document.getElementById('main');
+customElements.define('home-component', Home);
+
+const outlet = document.querySelector('.main');
 const router = new Router(outlet);
+
+router.setRoutes([
+    {path: '/', component: 'home-component'}
+]);
