40111226186165a1014260b748ae144e3f99b8fb
nikolaitrifonov0 <nikolaitrifonov0@gmail.com>
2020-12-15 20:07:15 +0200

    hi

---
+++ firebaseInit.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC_REBy7A1m53-m64RS0ncL5dtQ0cnet30",
+    authDomain: "tvtracker-c555d.firebaseapp.com",
+    projectId: "tvtracker-c555d",
+    storageBucket: "tvtracker-c555d.appspot.com",
+    messagingSenderId: "605881568803",
+    appId: "1:605881568803:web:7d501a139683ca240aab96"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
