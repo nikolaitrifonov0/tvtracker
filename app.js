2273706584dc7d0dfaaa3b6b9004285b9d21f110
nikolaitrifonov0 <nikolaitrifonov0@gmail.com>
2020-12-15 19:46:53 +0200

    added login template

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import {Router} from 'https://unpkg.com/@vaadin/router';
 import Home from './components/home.js';
+import Login from './components/login.js';
 
 customElements.define('home-component', Home);
+customElements.define('login-component', Login)
 
 const outlet = document.querySelector('.main');
 const router = new Router(outlet);
 
 router.setRoutes([
-    {path: '/', component: 'home-component'}
+    {path: '/', component: 'home-component'},
+    {path: '/login', component: 'login-component'},
 ]);
